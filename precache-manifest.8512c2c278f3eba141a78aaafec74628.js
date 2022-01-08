be70ba84005cc2b53d08bba4fc106a7a67874147
Sarthak Swaroop <sarthakswarup00@gmail.com>
2022-01-09 02:24:36 +0530

    Updates

---
+++ precache-manifest.8512c2c278f3eba141a78aaafec74628.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f1a044b2305f4e6854cbbd8e740ddb",
+    "url": "/project-corona-trackerr/index.html"
+  },
+  {
+    "revision": "54ea32b0018a5ad08dac",
+    "url": "/project-corona-trackerr/static/css/main.ea4bdb8a.chunk.css"
+  },
+  {
+    "revision": "d2efd3b333de11549898",
+    "url": "/project-corona-trackerr/static/js/2.889f2f09.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/project-corona-trackerr/static/js/2.889f2f09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54ea32b0018a5ad08dac",
+    "url": "/project-corona-trackerr/static/js/main.6836fdd3.chunk.js"
+  },
+  {
+    "revision": "84bf85a3c1621e0b905b",
+    "url": "/project-corona-trackerr/static/js/runtime-main.c3d2bccf.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/project-corona-trackerr/static/media/image.d7265326.png"
+  }
+]);
